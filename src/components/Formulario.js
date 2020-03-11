f659418541d722300af48f0c67ceb4ce805a4727
Chrystian Lozano <chrystianlozano@icloud.com>
2020-03-11 13:35:08 -0600

    validar Formulario :D

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Error from './Error'
 const Formulario = () => {
+
+    const[termino, guardarTermino] = useState('')
+    const[error, guardarError] = useState(false)
+
+    const buscarImagenes = e => {
+        e.preventDefault();
+        
+        //validar
+        if(termino.trim() === ''){
+            guardarError(true)
+            return
+        }
+
+        //enviar el termino de busqueda hacie el component eprincipal
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit = {buscarImagenes}
+        >
             <div className="row">
                 <div className="form-group col-md-8">
                     <input
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Buscar una imagen, ejemplo: futbol o café"
+                        onChange={e => guardarTermino(e.target.value)}
                     />
                 </div>
                 <div className="form-group col-md-4">
@@ -18,6 +38,7 @@ const Formulario = () => {
                     />
                 </div>                
             </div>
+            {error ? <Error mensaje="Agrega un término en la búsqueda"/> : null}
         </form>
 
      );
